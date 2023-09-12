a4491c5cbfb7126d8af8f892c7b901fb4f284fea
MATHEUS ALVES REIS DA SILVA <22282211@dsn127m1247443.cfp127aluno.local>
2023-09-12 16:50:45 -0300

    tela de registrar-se na fase final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation ("io.coil-kt:coil-compose:2.3.0")
+
+    //Mais icones
+    implementation ("androidx.compose.material:material-icons-extended")
+
+    implementation ("io.coil-kt:coil-compose:2.3.0")
 }
