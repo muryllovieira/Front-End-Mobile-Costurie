76a157f7aabf2c3df3ca6f09b59f3cdbdc8092ca
muryllovieira <muryllovieira59@gmail.com>
2023-09-16 17:41:34 -0300

    FINALMENTE CADASTRO SENDO REALIZADO

---
+++ app/src/main/java/br/senai/sp/jandira/costurie_app/service/RetrofitFactory.kt
@@ -0,0 +1,17 @@
+package br.senai.sp.jandira.costurie_app.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitFactory {
+
+    private const val BASE_URL = "https://super-hare-shoulder-pads.cyclic.cloud"
+
+    fun getInstance(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
