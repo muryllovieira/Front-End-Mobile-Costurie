c0410a4895ff9f4247b5f6d66d11c427abcf93e4
MURYLLO SIQUEIRA VIEIRA <22282224@dsn127m1247444.cfp127aluno.local>
2023-09-19 16:55:20 -0300

    Dando Inicio no Edit Profile

---
+++ app/src/main/java/br/senai/sp/jandira/costurie_app/screens/editProfile/EditProfile.kt
@@ -0,0 +1,92 @@
+package br.senai.sp.jandira.costurie_app.screens.editProfile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.senai.sp.jandira.costurie_app.R
+import br.senai.sp.jandira.costurie_app.ui.theme.Costurie_appTheme
+
+@Composable
+fun EditProfile() {
+
+    Costurie_appTheme {
+
+        Surface(
+            modifier = Modifier
+                .fillMaxSize(),
+            color = Color.White
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.forma_tela_perfil),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .height(240.dp)
+                            .width(390.dp),
+                        alignment = Alignment.TopStart
+                    )
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+
+                            Image(
+                                painter = painterResource(id = R.drawable.arrow_back),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(35.dp)
+                            )
+
+                            Image(
+                                painter = painterResource(id = R.drawable.save_icon),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(35.dp)
+                            )
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EditProfilePreview() {
+    EditProfile()
+}
