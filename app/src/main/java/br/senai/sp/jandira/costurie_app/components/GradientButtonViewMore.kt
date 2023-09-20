99486d11b2a2e29728b71f40f3ed1eb7bc2fe9d7
MATHEUS ALVES REIS DA SILVA <mts02501@gmail.com>
2023-09-20 16:59:07 -0300

    up

---
+++ app/src/main/java/br/senai/sp/jandira/costurie_app/components/GradientButtonViewMore.kt
@@ -0,0 +1,65 @@
+package br.senai.sp.jandira.costurie_app.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.costurie_app.ui.theme.Destaque1
+import br.senai.sp.jandira.costurie_app.ui.theme.Destaque2
+import br.senai.sp.jandira.costurie_app.ui.theme.ShapeButton
+
+@Composable
+fun GradientButtonViewMore (
+    onClick: () -> Unit,
+    text: String,
+    color1: Color,
+    color2: Color
+) {
+    Button(
+        onClick,
+        modifier = Modifier
+            .background(
+                Brush.horizontalGradient(
+                    colors = listOf(
+                        color1,
+                        color2
+                    )
+                ),
+                shape = ShapeButton.large,
+            )
+            .height(30.dp)
+            .width(110.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp,
+            hoveredElevation = 0.dp
+        )
+    ) {
+        Text(
+            text = text.uppercase(),
+            fontSize = 10.sp,
+            style = MaterialTheme.typography.bodySmall,
+            fontWeight = FontWeight.SemiBold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GradientButtonViewMorePreview() {
+    GradientButtonViewMore(onClick = {  }, text = "", color1 = Destaque1, color2 = Destaque2)
+}
