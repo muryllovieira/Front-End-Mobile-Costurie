19c8b129200041039a22132ebe7208d11cba6e10
ALEXSSANDRO DA SILVA GOMES <22282169@dsn127m1249387.cfp127aluno.local>
2023-10-04 16:26:53 -0300

    integracao de personalizacao em progresso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -69,6 +70,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -111,6 +113,7 @@ dependencies {
 
     //Firebase
     implementation ("com.google.firebase:firebase-storage:20.0.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.8.1")
 
     //SQL LITE DEPENDENCIAS
     implementation ("androidx.room:room-runtime:2.5.2")
